cbd9040f10818ca3fa2f58f1bb0bc366529ae804
Kalyan Chatterjee <kalyanchatterjee84@gmail.com>
2022-07-09 12:59:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,14 +11,14 @@ def hello_world():
    if request_type_str == "GET":
     return render_template('hi.html', href='static/Base.png')
    else:
-    # text = request.form["text"]
-    # model = load('app/model.joblib')
-    # vector= load('app/vector.joblib')
-    # pred= model.predict(vector.transform([text]))[0]
-    # if pred == 0:
-    #   return render_template('hi.html', href='app/static/negative.png')
-    # else:
-    return render_template('hi.html', href='static/positive.png')
+    text = request.form["text"]
+    model = load('app/model.joblib')
+    vector= load('app/vector.joblib')
+    pred= model.predict(vector.transform([text]))[0]
+    if pred == 0:
+      return render_template('hi.html', href='app/static/negative.png')
+    else:
+      return render_template('hi.html', href='static/positive.png')
 
 
 if __name__ == '__main__':
